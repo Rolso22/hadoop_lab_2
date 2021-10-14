d56603afe4e849cf90064fbe777218baa0a7b0d7
ramagf <ramagf@yandex.ru>
2021-10-14 17:16:00 +0300

    Scripted auto-commit on change (2021-10-14 17:16:00) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/AirportsMapper.java
+++ src/main/java/ru/bmstu/hadoop/labs/AirportsMapper.java
@@ -8,14 +8,15 @@ import java.io.IOException;
 
 import static ru.bmstu.hadoop.labs.Constants.*;
 
-public class AirportsMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+public class AirportsMapper extends Mapper<LongWritable, Text, AirportComparable, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
         String[] columns = value.toString().split("\",\"");
         if (key.get() > 0) {
             int airportId = Integer.parseInt(columns[CODE_INDEX].replaceAll("\"", ""));
-            String airportName = columns[DESCRIPTION_INDEX].replaceAll("")
+            String airportName = columns[DESCRIPTION_INDEX].replaceAll("\"", "");
+            context.write(new AirportComparable(airportId, AIRPORT_FLAG), new Text(airportName));
         }
     }
 }
