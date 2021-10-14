f52c9b5080a56d9616a160b483193946914a3416
ramagf <ramagf@yandex.ru>
2021-10-14 16:53:12 +0300

    Scripted auto-commit on change (2021-10-14 16:53:12) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/AirportComparator.java
+++ src/main/java/ru/bmstu/hadoop/labs/AirportComparator.java
@@ -3,7 +3,7 @@ package ru.bmstu.hadoop.labs;
 import org.apache.hadoop.io.WritableComparator;
 
 public class AirportComparator extends WritableComparator {
-    public AirportComparator() {
+    protected AirportComparator() {
         super(AirportComparable.class);
     }
 
