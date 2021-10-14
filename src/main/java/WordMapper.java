95626ec79774f4d7136ae524b8c37dcee62d25c2
ramagf <ramagf@yandex.ru>
2021-10-14 14:14:43 +0300

    Scripted auto-commit on change (2021-10-14 14:14:43) by gitwatch.sh

--- src/main/java/WordMapper.java
+++ src/main/java/WordMapper.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class WordMapper {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import java.io.IOException;
+
+public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException,
+            InterruptedException {
+        String inputStr = value.toString();
+        inputStr = inputStr.replaceAll("[\\p{P}\\p{S}]", "");
+        String[] words = inputStr.split(" ");
+        for (String word : words) {
+            word = word.toLowerCase();
+            context.write(new Text(word), new IntWritable(1));
+        }
+    }
 }
