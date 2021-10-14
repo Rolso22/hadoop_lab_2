9ad3c1ddb0af3f4236945ee9002cbdceb5cf7bb9
ramagf <ramagf@yandex.ru>
2021-10-14 17:19:56 +0300

    Scripted auto-commit on change (2021-10-14 17:19:56) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/AirportsMapper.java
+++ src/main/java/ru/bmstu/hadoop/labs/AirportsMapper.java
@@ -1,6 +1,5 @@
 package ru.bmstu.hadoop.labs;
 
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
