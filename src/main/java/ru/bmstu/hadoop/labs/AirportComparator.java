9e77f6b93d2ac68efe7ac7b111b3ce6a11e2ffdc
ramagf <ramagf@yandex.ru>
2021-10-14 16:15:09 +0300

    Scripted auto-commit on change (2021-10-14 16:15:09) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/AirportComparator.java
+++ src/main/java/ru/bmstu/hadoop/labs/AirportComparator.java
@@ -4,10 +4,11 @@ import org.apache.hadoop.io.WritableComparator;
 
 public class AirportComparator extends WritableComparator {
     public AirportComparator() {
+        super();
     }
 
-    @Override
-    public int compare(Object a, Object b) {
-        return Integer.compare(a, b);
-    }
+//    @Override
+//    public int compare(Object a, Object b) {
+//        return Integer.compare(a, b);
+//    }
 }
