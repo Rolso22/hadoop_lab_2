33cad6814670bbd887a35e3be017e4806606155e
ramagf <ramagf@yandex.ru>
2021-10-14 17:12:01 +0300

    Scripted auto-commit on change (2021-10-14 17:12:01) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/Constants.java
+++ src/main/java/ru/bmstu/hadoop/labs/Constants.java
@@ -5,4 +5,6 @@ public class Constants {
     public static final int AIRPORT_INDEX = 14;
     public static final int AIRPORT_FLAG = 0;
     public static final int FLIGHT_FLAG = 1;
+    public static final int CODE_INDEX = 0;
+    public static final int DESCRIPTION_INDEX = 1;
 }
