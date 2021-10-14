15f590616940697b11d326b082ac8a2647071b60
ramagf <ramagf@yandex.ru>
2021-10-14 14:41:27 +0300

    Scripted auto-commit on change (2021-10-14 14:41:27) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/WritableCSV.java
+++ src/main/java/ru/bmstu/hadoop/labs/WritableCSV.java
@@ -1,2 +1,20 @@
-package ru.bmstu.hadoop.labs;public class WritableCSV {
+package ru.bmstu.hadoop.labs;
+
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class WritableCSV implements Writable {
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
