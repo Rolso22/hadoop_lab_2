def24d57f3fbc0682baacd448b74fec7da5c1a85
ramagf <ramagf@yandex.ru>
2021-10-14 17:25:17 +0300

    Scripted auto-commit on change (2021-10-14 17:25:17) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/FlightsMapper.java
+++ src/main/java/ru/bmstu/hadoop/labs/FlightsMapper.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 import static ru.bmstu.hadoop.labs.Constants.*;
 
-public class FlightsMapper extends Mapper<LongWritable, Text, AirportComparable, FloatWritable> {
+public class FlightsMapper extends Mapper<LongWritable, Text, AirportComparable, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
@@ -18,9 +18,8 @@ public class FlightsMapper extends Mapper<LongWritable, Text, AirportComparable,
             if (delayTime.equals("")) {
                 return;
             }
-            float delay = Float.parseFloat(delayTime);
             int airportId = Integer.parseInt(columns[AIRPORT_INDEX]);
-            context.write(new AirportComparable(airportId, AIRPORT_FLAG), new FloatWritable(delay));
+            context.write(new AirportComparable(airportId, AIRPORT_FLAG), new Text(delayTime));
         }
     }
 }
