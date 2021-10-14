d96b97bda416e7bffd914f9dd51b1cf3ac4e8b1a
ramagf <ramagf@yandex.ru>
2021-10-14 16:45:31 +0300

    Scripted auto-commit on change (2021-10-14 16:45:31) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/Constants.java
+++ src/main/java/ru/bmstu/hadoop/labs/Constants.java
@@ -3,4 +3,6 @@ package ru.bmstu.hadoop.labs;
 public class Constants {
     public static final int DELAY_TIME_INDEX = 18;
     public static final int AIRPORT_INDEX = 14;
+    public static final int AIRPORT_FLAG = 14;
+    public static final int FLIGHT_FLAG = 14;
 }
