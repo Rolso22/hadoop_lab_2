8aab1886252b8511eac083572bd6dfe2e1ddd86f
ramagf <ramagf@yandex.ru>
2021-10-14 15:57:58 +0300

    Scripted auto-commit on change (2021-10-14 15:57:58) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/AirportApp.java
+++ src/main/java/ru/bmstu/hadoop/labs/AirportApp.java
@@ -12,7 +12,8 @@ import org.apache.hadoop.mapreduce.Job;
 public class AirportApp {
     public static void main(String[] args) throws Exception {
         if (args.length != 3) {
-            
+            System.err.println("Usage: AirportApp <input path> <output path>");
+            System.exit(-1);
         }
         Job job = Job.getInstance();
         job.setJarByClass(AirportApp.class);
