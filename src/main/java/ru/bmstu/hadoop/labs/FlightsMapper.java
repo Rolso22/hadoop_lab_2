7cc82e437ff357941eae66d21290bc8ab306d418
ramagf <ramagf@yandex.ru>
2021-10-15 17:27:27 +0300

    Scripted auto-commit on change (2021-10-15 17:27:27) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/FlightsMapper.java
+++ src/main/java/ru/bmstu/hadoop/labs/FlightsMapper.java
@@ -14,12 +14,11 @@ public class FlightsMapper extends Mapper<LongWritable, Text, AirportComparable,
         String[] columns = value.toString().split(",");
         if (key.get() > 0) {
             String delayTime = columns[DELAY_TIME_INDEX];
-            if (delayTime.equals("")) {
-                return;
+            if (!delayTime.equals("")) {
+                int airportId = Integer.parseInt(columns[AIRPORT_INDEX]);
+                float delay = Float.parseFloat(delayTime);
+                if (delay > 0.0f) context.write(new AirportComparable(airportId, FLIGHT_FLAG), new Text(delayTime));
             }
-            int airportId = Integer.parseInt(columns[AIRPORT_INDEX]);
-            float delay = Float.parseFloat(delayTime);
-            if (delay > 0.0f) context.write(new AirportComparable(airportId, FLIGHT_FLAG), new Text(delayTime));
         }
     }
 }
