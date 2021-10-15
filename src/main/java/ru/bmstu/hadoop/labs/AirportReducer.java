898704db00ef03ae5dd2a5c22e4a51cf715dedce
ramagf <ramagf@yandex.ru>
2021-10-15 15:25:14 +0300

    Scripted auto-commit on change (2021-10-15 15:25:14) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/AirportReducer.java
+++ src/main/java/ru/bmstu/hadoop/labs/AirportReducer.java
@@ -4,9 +4,13 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 import javax.xml.soap.Text;
 
+import java.util.Iterator;
+
 import static ru.bmstu.hadoop.labs.Constants.*;
 
 public class AirportReducer extends Reducer {
     @Override
-    protected void reduce(AirportComparable key, Text value, Text )
+    protected void reduce(AirportComparable key, Iterator<Text> value, Context context) {
+        
+    }
 }
