8e4b45787fe9d57be40ea69b3c7afde19cde9acb
ramagf <ramagf@yandex.ru>
2021-10-15 16:15:35 +0300

    Scripted auto-commit on change (2021-10-15 16:15:34) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/AirportReducer.java
+++ src/main/java/ru/bmstu/hadoop/labs/AirportReducer.java
@@ -2,18 +2,24 @@ package ru.bmstu.hadoop.labs;
 
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
+import java.io.IOException;
 import java.util.Iterator;
 
 import static ru.bmstu.hadoop.labs.Constants.*;
 
 public class AirportReducer extends Reducer<AirportComparable, Text, Text, Text> {
+    public AirportReducer() {
+        super();
+    }
+
     @Override
-    protected void reduce(AirportComparable key, Iterator<Text> values, Context context) {
+    protected void reduce(AirportComparable key, Iterator<Text> values, Context context) throws IOException, InterruptedException {
         String airportName = values.next().toString();
         float max = Float.MIN_VALUE;
         float min = Float.MAX_VALUE;
         int count = 0;
         float sum = 0;
+        if (!values.hasNext()) return;
         while (values.hasNext()) {
             float val = Float.parseFloat(values.next().toString());
             if (max < val) max = val;
@@ -21,6 +27,6 @@ public class AirportReducer extends Reducer<AirportComparable, Text, Text, Text>
             sum += val;
             count++;
         }
-        
+        context.write(new Text(airportName), new Text("min: " + min + "\n" + "max: " + max + "\n" + "average: " + sum/count));
     }
 }
