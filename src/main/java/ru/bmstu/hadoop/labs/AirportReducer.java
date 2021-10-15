5dfdf13115ea62fab486d77133e726ec44637f26
ramagf <ramagf@yandex.ru>
2021-10-15 16:12:25 +0300

    Scripted auto-commit on change (2021-10-15 16:12:25) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/AirportReducer.java
+++ src/main/java/ru/bmstu/hadoop/labs/AirportReducer.java
@@ -10,8 +10,17 @@ public class AirportReducer extends Reducer<AirportComparable, Text, Text, Text>
     @Override
     protected void reduce(AirportComparable key, Iterator<Text> values, Context context) {
         String airportName = values.next().toString();
-        int max = Integer.MAX_VALUE;
-        int min = Integer.MIN_VALUE;
+        float max = Float.MIN_VALUE;
+        float min = Float.MAX_VALUE;
+        int count = 0;
+        float sum = 0;
+        while (values.hasNext()) {
+            float val = Float.parseFloat(values.next().toString());
+            if (max < val) max = val;
+            if (min > val) min = val;
+            sum += val;
+            count++;
+        }
         
     }
 }
