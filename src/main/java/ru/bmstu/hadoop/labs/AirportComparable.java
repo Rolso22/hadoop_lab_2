ab3b0a59f74d57cde87e922240a8f8a63de9a88f
ramagf <ramagf@yandex.ru>
2021-10-15 17:51:12 +0300

    Scripted auto-commit on change (2021-10-15 17:51:12) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/AirportComparable.java
+++ src/main/java/ru/bmstu/hadoop/labs/AirportComparable.java
@@ -41,7 +41,7 @@ public class AirportComparable implements WritableComparable {
 
     @Override
     public void readFields(DataInput dataInput) throws IOException {
-        airportId = dataInput.readInt();
         flag = dataInput.readInt();
+        airportId = dataInput.readInt();
     }
 }
