462d91c9a74051d39aa902d6ff776bc3beb84622
ramagf <ramagf@yandex.ru>
2021-10-15 15:18:51 +0300

    Scripted auto-commit on change (2021-10-15 15:18:50) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/AirportPartitioner.java
+++ src/main/java/ru/bmstu/hadoop/labs/AirportPartitioner.java
@@ -8,7 +8,7 @@ import static ru.bmstu.hadoop.labs.Constants.*;
 public class AirportPartitioner extends Partitioner<AirportComparable, Text> {
 
     @Override
-    public int getPartition(AirportComparable airportComparable, Text text, int i) {
-        return 0;
+    public int getPartition(AirportComparable airportComparable, Text text, int numTasks) {
+        return airportComparable.getAirportId() %  
     }
 }
