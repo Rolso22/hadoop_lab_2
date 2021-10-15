dcd6f8fd8f389c3437179a2ce788d3cabbf9e815
ramagf <ramagf@yandex.ru>
2021-10-15 16:17:09 +0300

    Scripted auto-commit on change (2021-10-15 16:17:09) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/AirportReducer.java
+++ src/main/java/ru/bmstu/hadoop/labs/AirportReducer.java
@@ -13,8 +13,21 @@ public class AirportReducer extends Reducer<AirportComparable, Text, Text, Text>
     }
 
     @Override
-    protected void reduce(AirportComparable key, Iterable<Text> values, Reducer<AirportComparable, Text, Text, Text>.Context context) throws IOException, InterruptedException {
-        super.reduce(key, values, context);
+    protected void reduce(AirportComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        String airportName = values.next().toString();
+        float max = Float.MIN_VALUE;
+        float min = Float.MAX_VALUE;
+        int count = 0;
+        float sum = 0;
+        if (!values.hasNext()) return;
+        while (values.hasNext()) {
+            float val = Float.parseFloat(values.next().toString());
+            if (max < val) max = val;
+            if (min > val) min = val;
+            sum += val;
+            count++;
+        }
+        context.write(new Text(airportName), new Text("min: " + min + "\n" + "max: " + max + "\n" + "average: " + sum/count));
     }
 
     @Override
