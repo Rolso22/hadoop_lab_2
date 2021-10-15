f64ad108f5b5a31285ff04f55d787ef1f0eaaf9f
ramagf <ramagf@yandex.ru>
2021-10-15 15:24:44 +0300

    Scripted auto-commit on change (2021-10-15 15:24:44) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/AirportReducer.java
+++ src/main/java/ru/bmstu/hadoop/labs/AirportReducer.java
@@ -2,8 +2,11 @@ package ru.bmstu.hadoop.labs;
 
 import org.apache.hadoop.mapreduce.Reducer;
 
+import javax.xml.soap.Text;
+
 import static ru.bmstu.hadoop.labs.Constants.*;
 
 public class AirportReducer extends Reducer {
-    
+    @Override
+    protected void reduce(AirportComparable key, Text value, Text )
 }
